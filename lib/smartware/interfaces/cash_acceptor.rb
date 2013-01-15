dab43345feff06ebf878e2225da33865e2373986
Sergey Gridassov <grindars@gmail.com>
2013-01-15 16:44:40 +0400

    Made CashAcceptor more robust and less CPU-hungry.

--- lib/smartware/interfaces/cash_acceptor.rb
+++ lib/smartware/interfaces/cash_acceptor.rb
@@ -5,121 +5,169 @@ module Smartware
   module Interface
     class CashAcceptor < Interface
 
-      attr_reader :status, :banknotes
-
       def initialize(config)
         super
 
-        #
-        # Init vars
-        #
-        @configured = false
-        @no_limit = true
-        @banknotes = {}
-        @commands = %w(monitor)
-        @status = { :cassette => true }
-        @device.cancel_accept
-        @session = Thread.new &method(:poll)
+        @limit = nil
+        @banknotes = Hash.new do |hash, key|
+          hash[key] = 0
+        end
+
+        @status_mutex = Mutex.new
+        @status = {
+          casette: false,
+          accepting: false,
+          error: '',
+          model: '',
+          version: ''
+        }
+
+        @commands = Queue.new
+        @commands.push :close
+
+        Thread.new &method(:dispatch_commands)
+        Thread.new &method(:periodic)
         Smartware::Logging.logger.info "Cash acceptor monitor started"
-        @configured = true
-      rescue => e
-        @configured = false
-        Smartware::Logging.logger.error e.message
-        Smartware::Logging.logger.error e.backtrace.join("\n")
       end
 
       def open_session(limit_min, limit_max)
-        @commands << 'open'
+
         @banknotes = {}
-        if !limit_min.nil? && !limit_max.nil?
-          @no_limit = false
-          @limit_min = limit_min
-          @limit_max = limit_max
+
+        if limit_min.nil? || limit_max.nil?
+          @limit = nil
         else
-          @no_limit = true
+          @limit = limit_min..limit_max
         end
+
+        @commands.push :open
+        @commands.push :get_money
       end
 
       def close_session
-        @commands << 'close'
-        @limit_min = nil
-        @limit_max = nil
-        @no_limit = true
+        @commands.push :close
+        @limit = nil
       end
 
       def configured?
-        @configured
+        true
       end
 
       def error
-        @status[:error] || ''
+        self.status[:error]
       end
 
       def model
-        @status[:model]
+        self.status[:model]
       end
 
       def version
-        @status[:version]
+        self.status[:version]
+      end
+
+      def accepting
+        self.status[:accepting]
+      end
+
+      def status
+        @status_mutex.synchronize { @status }
+      end
+
+      def banknotes
+        @status_mutex.synchronize { @banknotes }
       end
 
       def cashsum
-        @banknotes.inject(0) do |result, (key, value)|
+        self.banknotes.inject(0) do |result, (key, value)|
           result + key.to_i * value.to_i
         end
       end
 
-
-      #
-      # Session private method
-      #
-
       private
 
-      def poll
-        loop do
-          case @commands[0]
-            when 'open'
-              @device.reset
-              @device.accept
-              Smartware::Logging.logger.info "Cash acceptor open: #{@commands}"
+      def limit_satisfied(sum)
+        @limit.nil? or @limit.include? sum
+      end
 
-              @commands.shift
-              @commands << 'get_money'
-            when 'get_money'
-              res = @device.current_banknote
-              if res.is_a? Integer # Have a banknote
-                if @no_limit or (@limit_min..@limit_max).include?(self.cashsum + res)
-                  @device.stack
-                  @banknotes[res] = (@banknotes[res]||0) + 1
-                  Smartware::Logging.logger.info "Cash acceptor bill stacked, #{res}"
-                else
-                  @device.return
-                  Smartware::Logging.logger.info "Cash acceptor limit violation, return #{res}"
-                end
-              elsif res.is_a? String # Have a error, errors always as String
-                Smartware::Logging.logger.error "Cash acceptor error #{res}"
-                @status[:error] = res
-              end
-              @device.cancel_accept if !@no_limit && (@limit_max == self.cashsum and @limit_max > 0) # Close cash acceptor if current cashsum equal max-limit
+      def execute_open
+        @device.reset
+        @device.accept
+        Smartware::Logging.logger.info "Cash acceptor open"
 
-              @commands.shift
-              @commands << 'get_money' if @commands.empty?
-            when 'close'
-              @device.cancel_accept
-              Smartware::Logging.logger.info "Cash acceptor close: #{@commands}"
+        @status_mutex.synchronize do
+          @status[:accepting] = true
+        end
+      end
 
-              @commands.shift
-            when 'monitor'
-              @status[:error] = @device.error || ''
-              @status[:model] = @device.model
-              @status[:version] = @device.version
-              @status[:cassette] = @device.cassette?
-              @commands.shift
-            else
-              @commands << 'monitor'
+      def execute_get_money
+        res = @device.current_banknote
+
+        case res
+        when Integer
+          if limit_satisfied(self.cashsum + res)
+            @device.stack
+            @status_mutex.synchronize do
+              @banknotes[res] += 1
+            end
+            Smartware::Logging.logger.info "Cash acceptor bill stacked, #{res}"
+          else
+            @device.return
+            Smartware::Logging.logger.info "Cash acceptor limit violation, return #{res}"
           end
-          sleep 0.5
+
+        when String
+          Smartware::Logging.logger.error "Cash acceptor error #{res}"
+
+          @status_mutex.synchronize do
+            @status[:error] = res
+          end
+        end
+
+        if !@limit.nil? && @limit.end > 0 && @limit.end == self.cashsum
+          # Close cash acceptor if current cashsum equal max-limit
+
+          execute_close
+        end
+      end
+
+      def execute_close
+        @device.cancel_accept
+        Smartware::Logging.logger.info "Cash acceptor close"
+
+        @status_mutex.synchronize do
+          @status[:accepting] = false
+        end
+      end
+
+      def execute_monitor
+        status = self.status
+
+        status[:error] = @device.error || ''
+        status[:model] = @device.model
+        status[:version] = @device.version
+        status[:cassette] = @device.cassette?
+
+        self.status = status
+      end
+
+      def dispatch_commands
+        loop do
+          command = @commands.pop
+
+          send :"execute_#{command}"
+        end
+      end
+
+      def periodic
+        loop do
+          @commands.push :monitor
+
+          if self.accepting
+            @commands.push :get_money
+            sleep 0.5
+          else
+            sleep 5
+          end
         end
       end
     end
