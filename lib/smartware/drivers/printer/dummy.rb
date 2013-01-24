069f625f980217dda36dac4245ca7c3370c584f9
Sergey Gridassov <grindars@gmail.com>
2013-01-24 10:31:52 +0400

    Dummy printer will create files now.

--- lib/smartware/drivers/printer/dummy.rb
+++ lib/smartware/drivers/printer/dummy.rb
@@ -25,6 +25,13 @@ module Smartware
         end
 
         def print(data)
+          name = "smartwareprint_#{Time.now.strftime("%Y-%m-%d-%H:%M:%S")}.txt"
+          pathname = File.join(Dir.home, name)
+
+          File.open(pathname, 'w') { |io| io.write data }
+
+          Logging.logger.info "Created #{pathname}"
+
           true
         end
 
@@ -33,10 +40,23 @@ module Smartware
         end
 
         def new_render
-          Redcarpet::Render::HTML.new
+          DummyRender.new
         end
       end
 
+      class DummyRender < Redcarpet::Render::Base
+        def linebreak
+          "\n"
+        end
+
+        def normal_text(text, keep_newlines = false)
+          unless keep_newlines
+            text.gsub! "\n", " "
+          end
+
+          text
+        end
+      end
     end
   end
 end
