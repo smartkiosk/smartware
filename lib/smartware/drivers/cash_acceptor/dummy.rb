1716d61da4a8e95a7aa266cade006c3933044930
Sergey Gridassov <grindars@gmail.com>
2013-01-29 20:51:20 +0400

    Improved dummy cash acceptor.

--- lib/smartware/drivers/cash_acceptor/dummy.rb
+++ lib/smartware/drivers/cash_acceptor/dummy.rb
@@ -10,7 +10,9 @@ module Smartware
         attr_accessor :enabled_types
 
         def initialize(config)
-          @bill_types = []
+          @bill_types = [ 10, 50, 100, 500, 5000 ].map do |value|
+            Interface::CashAcceptor::BillType.new(value, 'RUS')
+          end
 
           @open = nil
           @closed = nil
@@ -18,8 +20,13 @@ module Smartware
           @stacked = nil
           @returned = nil
           @status = nil
+          @escrow_bill = nil
 
-          @enabled_types = nil
+          @enabled_types = 0
+
+          EventMachine.add_periodic_timer 0.5, method(:poll)
+
+          @dummy_state = :power_up
         end
 
         def model
@@ -33,8 +40,57 @@ module Smartware
         def accepting?
           false
         end
-      end
 
+        private
+
+        def poll
+          case @dummy_state
+          when :power_up
+            @dummy_state = :initialize
+
+          when :initialize
+            @dummy_state = :disabled
+
+          when :disabled
+            if @enabled_types != 0
+              @dummy_state = :idle
+              @open.call
+            end
+
+          when :idle
+            if @enabled_types == 0
+              @dummy_state = :disabled
+              @close.call
+            else
+              bill, index = @bill_types.each_with_index
+                                       .select { |(obj, index)| enabled_types & (1 << index) != 0 }
+                                       .sample
+
+
+              @dummy_state = :escrow
+              @escrow_bill = bill
+            end
+
+          when :escrow
+            if @escrow.call(@escrow_bill)
+              @dummy_state = :stacking
+            else
+              @dummy_state = :returning
+            end
+
+          when :stacking
+            @dummy_state = :idle
+            @stacked.call @escrow_bill
+
+          when :returning
+            @dummy_state = :idle
+            @return.call @escrow_bill
+
+          end
+
+          @status.call nil
+        end
+      end
     end
   end
 end
